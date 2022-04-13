1f70f47f0e3111b66c2c6686a4dbe35fc7b2ae2e
IanGaplichnik <ian.gaplichnik@protonmail.ch>
2022-04-13 21:45:55 +0300

    ex 1.1-1.2 done, next commits will represent next exercises

--- part1/example/example/src/App.js
+++ part1/example/example/src/App.js
@@ -1,50 +1,28 @@
 import { useState } from 'react'
 
-const History = (props) => {
-	if (props.allClicks.length === 0) {
-		return (
-			<div>
-				The app is used by pressing buttons
-			</div>
-		)
-	}
-	return (
-		<div>
-			Button press history: {props.allClicks.join(' ')}
-		</div>
-	)
-}
+const Display = (props) => <div>{props.value}</div>
 
-const Button = ({ handleClick, text }) => (
-	<button onClick={handleClick}>
-		{text}
+const Button = (props) => ( 
+	<button onClick={props.handleClick}>
+		{props.text}
 	</button>
 )
 
 const App = () => {
-	const [left, setLeft] = useState(0);
-	const [right, setRight] = useState(0);
-	const [allClicks, setAll] = useState([]);
-
-	const handleLeftClick = () => {
-		setAll(allClicks.concat('L'));
-		setLeft(left + 1);
-	}
-
-	const handleRightClick = () => {
-		debugger;
-		setAll(allClicks.concat('R'));
-		setRight(right + 1);
-	}
-
-	return (
-		<div>
-			{left}
-			<Button handleClick={handleLeftClick} text="Left" />
-			<Button handleClick={handleRightClick} text="Right" />
-			{right}
-			<History allClicks={allClicks} />
-		</div>
-	)
+  const [value, setValue] = useState(10)
+	
+	const setToValue = newValue => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+  
+  return (
+    <div>
+			<Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="zero"/>
+      <Button handleClick={() => setToValue(value + 1)} text="increment"/>
+    </div>
+  )
 }
 export default App
