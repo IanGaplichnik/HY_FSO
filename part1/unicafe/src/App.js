f70a7c093bf66116dba0e0e24b0b12ca6c4e6b3b
IanGaplichnik <ian.gaplichnik@protonmail.ch>
2022-04-23 16:12:26 +0300

    completed unicafe exercise,cleaned up repo

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,49 +1,61 @@
 import { useState } from 'react'
 
-
 const Button = ({text, handleClick }) => (
 	<button onClick={handleClick}>
 		{text}
 	</button>
 )
 
+const StatisticLine = ({ text, value }) => (
+	<tr>
+		<td>
+			{text} 
+		</td>
+		<td>
+			{value}
+		</td>
+	</tr>
+)
+
 const Statistics = ({good, bad, neutral, all, avg, pos}) => {
 	if (all === 0) {
 		return (
-			<h1>No gathered statistics yet</h1>
+			<p>No gathered statistics yet</p>
 		)
 	}
 	return (
-		<div>
-			<h1>Statistics</h1>
-			<div>good {good}</div>
-			<div>neutral {neutral}</div>
-			<div>bad {bad}</div>
-			<div>all {all}</div>
-			<div>average {avg}</div>
-			<div>positive {pos}%</div>
-		</div>
+			<table>
+				<tbody>
+					<StatisticLine text="good" value={good} />
+					<StatisticLine text="neutral" value={neutral} />
+					<StatisticLine text="bad" value={bad} />
+					<StatisticLine text="all" value={all} />
+					<StatisticLine text="average" value={avg} />
+					<StatisticLine text="positive" value={pos + '%'} />
+				</tbody>
+			</table>
 	)
 }
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
+
 	const all = good + neutral + bad;
 	const avg = (good - bad) / all;
 	const pos = (good / all) * 100;
 
-  return (
-    <div>
+	return (
+		<div>
 			<h1>Give feedback</h1>
 			<Button text="good" handleClick={() => setGood(good + 1)}/>
 			<Button text="neutral" handleClick={() => setNeutral(neutral + 1)}/>
 			<Button text="bad" handleClick={() => setBad(bad + 1)}/>
+			<h1>Statistics</h1>
 			<Statistics good={good} neutral={neutral} bad={bad} all={all} avg={avg} pos={pos}/>
 		</div>
-  )
+	)
 }
 
 export default App
